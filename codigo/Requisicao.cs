6ec0c41b3f74eceed2b5f95db67abd7ed5fac2bf
João Victor <127231260+joaovictortbm@users.noreply.github.com>
2024-04-07 10:42:46 -0300

    Adição do código da classe Requisição

---
+++ codigo/Requisicao.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseRequisicaoPOO
+{
+    internal class Requisicao:Cliente
+    {
+        private Cliente cliente;
+        private int quantPessoas;
+
+        public Requisicao(Cliente cliente, int quantAcompanhantes) : base(cliente.Nome, cliente.Id, cliente.Cpf)
+        {
+            this.cliente = cliente;
+            this.quantPessoas = quantAcompanhantes;
+        }
+
+        public string entrada()
+        {
+            DateTime dataHoraEntrada = DateTime.Now;
+            Restaurante newCliente = new Restaurante();
+            return newCliente.requisicaoEntrada(this, dataHoraEntrada);
+
+        }
+        public string saida()
+        {
+            DateTime dataHoraSaida = DateTime.Now;
+            Restaurante newCliente = new Restaurante();
+            return newCliente.requisicaoSaida(this, dataHoraSaida);
+        }
+
+        public int obterQuantPessoas()
+        {
+            if (quantPessoas > 0)
+            {
+                return quantPessoas;
+            }
+            else
+            {
+                throw new Exception("Quantidade de pessoas inváido");
+            }
+        }
+    }
+}
