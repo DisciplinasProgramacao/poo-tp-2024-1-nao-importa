ece63816a3c4c6802adcd55f46cfdc23ff58eadf
Gustavo Rodrigues <1444365@sga.pucminas.br>
2024-04-08 21:43:35 -0300

    Classe Mesa

---
+++ codigo/Mesa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurante
+{
+    class Mesa
+    {
+        private int numMesa;
+        private bool disponivel;
+        private int capacidade;
+
+        public Mesa ( int numMesa, bool disponivel, int capacidade)
+        {
+            this.numMesa = numMesa;
+            this.disponivel = disponivel;
+            this.capacidade = capacidade;
+
+        }
+        public string relatorio()
+        {
+            return "teste";
+        }
+
+        public bool verificarAdequacao( Requisicao requisicao )
+        {
+            bool resposta = false;
+            if ( disponivel == true && requisicao.numAcompanhante <= capacidade)
+            {
+                resposta = true;
+            }
+
+            return resposta;
+        }
+
+
+    }
+}
