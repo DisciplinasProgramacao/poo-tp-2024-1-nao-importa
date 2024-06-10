f9b862a20275999a3ad4a64f157cd475806751a4
João José Cardoso Ribeiro <joaojosecr@gmail.com>
2024-06-10 09:25:18 -0300

    Item

---
+++ codigo/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseRequisicaoPOO
+{
+    internal class Item
+    {
+        protected int valor;
+        protected string nome;
+        
+        public int getValor()
+        {
+            return valor;
+            
+        }
+        public string getNome() {
+            return nome;
+        }
+    }
+}
