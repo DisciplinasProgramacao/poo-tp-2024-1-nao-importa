a6c5969f2e1fc0632961c0ea856d8923ecca98aa
Gustavo Campos Rodrigues <127527707+gustavocamposs@users.noreply.github.com>
2024-06-26 23:40:11 -0300

    Update Cafeteria.cs

--- codigo/ClasseRequisicaoPOO/ClasseRequisicaoPOO/Cafeteria.cs
+++ codigo/ClasseRequisicaoPOO/ClasseRequisicaoPOO/Cafeteria.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +8,7 @@ namespace ClasseRequisicaoPOO
 {
     internal  class Cafeteria : Estabelecimento
     {
-        public List<Requisicao> listaRequisicao;
+        private List<Requisicao> listaRequisicao;
 
         public Cafeteria()
         {
@@ -16,7 +16,7 @@ namespace ClasseRequisicaoPOO
         }
         public override string fecharConta(Requisicao cliente)
         {
-            return cliente.pedido.relatorio();
+            return cliente.getPedido().relatorio();
         }
 
 
@@ -30,7 +30,7 @@ namespace ClasseRequisicaoPOO
                 sb.Append(" - ");
                 sb.Append(listaRequisicao[i].getNome());
                 sb.Append("\n Pedido: ");
-                sb.Append(listaRequisicao[i].pedido.relatorio());
+                sb.Append(listaRequisicao[i].getPedido().relatorio());
             }
 
             return sb.ToString();
@@ -40,7 +40,7 @@ namespace ClasseRequisicaoPOO
         public override string relatorioRequisicao(Requisicao clienteSaindo)
         {
             string retorno = "";
-            retorno += "\n" + clienteSaindo.getNome() + " saindo do restaurante \n " + "Data e hora de entrada: " + clienteSaindo.dataHoraEntrada.ToString() +
+            retorno += "\n" + clienteSaindo.getNome() + " saindo do restaurante \n " + "Data e hora de entrada: " + clienteSaindo.getdataHoraEntrada().ToString() +
                 "\n Data e hora de saída: " + DateTime.Now.ToString();
             return retorno;
         }
@@ -54,6 +54,46 @@ namespace ClasseRequisicaoPOO
         {
             listaRequisicao.Remove(cliente);
             Console.WriteLine(relatorioRequisicao(cliente));
+        }
+
+        public override void mostrarMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("-------------------------------------");
+            Console.ResetColor();
+            Console.WriteLine("Bem-vindo à Cafeteria!");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("1. Chegada de Cliente");
+            Console.WriteLine("2. Fechar conta de cliente");
+            Console.WriteLine("3. Ver situação da cafeteria");
+            Console.WriteLine("4. Adicionar item a um pedido");
+            Console.WriteLine("0. Sair");
+            Console.WriteLine("-------------------------------------");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.Write("Escolha uma opção: ");
+            Console.WriteLine();
+        }
+
+        public override void mostrarItem()
+        {
+            Console.WriteLine("Comida:");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(" 1. Pão de queijo – R$ 5\n 2. Bolinha de cogumelo – R$ 7\n 3. Rissole de palmito – R$ 7\n 4. Coxinha de carne de jaca – R$ 4\n 5. Fatia de queijo de caju – R$ 9\n 6. Biscoito amanteigado – R$ 3\n 7. Cheesecake de frutas vermelhas - R$ 15");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Bebidas:");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(" 8. Água – R$ 3\n 9. Copo de suco – R$ 7\n 10. Café espresso orgânico R$ 6");
+            Console.ResetColor();
+        }
+
+        public List<Requisicao> getRequisicao()
+        {
+            return listaRequisicao;
         }
     }
 }
