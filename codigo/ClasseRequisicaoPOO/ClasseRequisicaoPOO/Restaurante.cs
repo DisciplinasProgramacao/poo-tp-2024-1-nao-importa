3cb1edfd13dc45a9d92a49bf5cac22b038289a6f
Gustavo Campos Rodrigues <127527707+gustavocamposs@users.noreply.github.com>
2024-06-26 23:34:03 -0300

    Update Restaurante.cs

--- codigo/ClasseRequisicaoPOO/ClasseRequisicaoPOO/Restaurante.cs
+++ codigo/ClasseRequisicaoPOO/ClasseRequisicaoPOO/Restaurante.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -67,7 +67,7 @@ namespace ClasseRequisicaoPOO
                 if (mesasUnicas[i].mesaEstaOcupada())
                 {
                     sb.Append(" - Esta ocupada\n");
-                    sb.Append(mesasUnicas[i].clienteSentado.pedido.relatorio());
+                    sb.Append(mesasUnicas[i].getclienteSentado().getPedido().relatorio());
                 }
                 else
                 {
@@ -109,7 +109,7 @@ namespace ClasseRequisicaoPOO
                     Console.WriteLine(RetirarNaMesa(listaDeMesa[i]));
                     Console.WriteLine(verificarFila(listaDeMesa[i]));
                     Console.WriteLine(relatorioRequisicao(cliente));
-                    
+                    break;
                 }
             }
         }
@@ -122,7 +122,7 @@ namespace ClasseRequisicaoPOO
                 if (listaDeMesa[i].verificarAdequacao(cliente) == true)
                 {
                     resposta = true;
-                 
+                    break;
                 }
             }
             return resposta;
@@ -139,18 +139,19 @@ namespace ClasseRequisicaoPOO
         }
         private string ColocarNaMesa(Requisicao cliente)
         {
-            string retorno = null;
+            string retorno = "";
             if (verificarDisponibilidade(cliente) == true)
             {
                 for (int i = 0; i < listaDeMesa.Count; i++)
                 {
-                    if (listaDeMesa[i].mesaEstaOcupada() == false && listaDeMesa[i].verificarAdequacao(cliente) == true && retorno == null)
+                    if (listaDeMesa[i].mesaEstaOcupada() == false && listaDeMesa[i].verificarAdequacao(cliente) == true)
                     {
                         listaDeMesa[i].setIdCliente(cliente.getId());
                         listaDeMesa[i].setEstaOcupada(true);
-                        listaDeMesa[i].clienteSentado = cliente;
+                        listaDeMesa[i].setclienteSentado(cliente);
                         retorno += "Cliente inserido em uma mesa\n";
-                        
+                        break;
+
                     }
                 }
             }
@@ -167,16 +168,16 @@ namespace ClasseRequisicaoPOO
         }
         public string verificarFila(Mesa mesa)
         {
-            string retorno = null;
+            string retorno = "";
             for (int i = 0; i < filaDeEspera.Count; i++)
             {
-                if (filaDeEspera[i].obterQuantPessoas() <= mesa.getCapacidade() && retorno == null);
+                if (filaDeEspera[i].obterQuantPessoas() <= mesa.getCapacidade()) ;
                 {
                     ColocarNaMesa(filaDeEspera[i]);
                     listaRequisicao.Add(filaDeEspera[i]);
                     retorno += filaDeEspera[i].getNome() + " foi direcionado da fila para o restaurante";
                     filaDeEspera.RemoveAt(i);
-                    
+                    break;
                 }
             }
             return retorno;
@@ -184,7 +185,7 @@ namespace ClasseRequisicaoPOO
         public override string relatorioRequisicao(Requisicao clienteSaindo)
         {
             string retorno = "";
-            retorno += "\n" + clienteSaindo.getNome() + " saindo do restaurante \n " + "Data e hora de entrada: " + clienteSaindo.dataHoraEntrada.ToString() +
+            retorno += "\n" + clienteSaindo.getNome() + " saindo do restaurante \n " + "Data e hora de entrada: " + clienteSaindo.getdataHoraEntrada().ToString() +
                 "\n Data e hora de saída: " + DateTime.Now.ToString();
             return retorno;
         }
@@ -196,8 +197,45 @@ namespace ClasseRequisicaoPOO
         }
         public override string fecharConta(Requisicao cliente)
         {
-            return cliente.pedido.relatorio();
+            return cliente.getPedido().relatorio();
         }
+
+        public override void mostrarMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("-------------------------------------");
+            Console.ResetColor();
+            Console.WriteLine("Bem-vindo ao restaurante!");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("1. Chegada de Cliente");
+            Console.WriteLine("2. Liberar Mesa");
+            Console.WriteLine("3. Ver situação das mesas");
+            Console.WriteLine("4. Ver fila de espera");
+            Console.WriteLine("5. Adicionar item a um pedido");
+            Console.WriteLine("0. Sair");
+            Console.WriteLine("-------------------------------------");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.Write("Escolha uma opção: ");
+            Console.WriteLine();
+        }
+
+        public override void mostrarItem()
+        {
+            Console.WriteLine("Comida:");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(" 1. Moqueca de Palmito – R$ 32\n 2. Falafel Assado – R$ 20\n 3. Salada Primavera com Macarrão Konjac – R$ 25\n 4. Escondidinho de Inhame – R$ 18\n 5. Strogonoff de Cogumelos – R$ 35\n 6. Caçarola de legumes – R$ 22");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Bebidas:");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(" 7. Água – R$ 3\n 8. Copo de suco – R$ 7\n 9. Refrigerante orgânico – R$ 7\n 10. Cerveja vegana – R$ 9\n 11. Taça de vinho vegano – R$ 18");
+            Console.ResetColor();
+        }
+
         public List<Mesa> getMesa()
         {
             return listaDeMesa;
